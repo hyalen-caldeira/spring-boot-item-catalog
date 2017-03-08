28d314f553bb0fc341932ecb54253b5a36a35a90
Hyalen Moreira <hyalen.moreira@gmail.com>
2017-03-07 18:31:57 -0800

    Item Catalog SpringBoot initial version

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringBoot Item Catalog
---
+++ src/main/java/us/hyalen/springbootprojects/ItemCatalogApp.java
@@ -0,0 +1,11 @@
+package us.hyalen.springbootprojects;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemCatalogApp {
+	public static void main(String[] args) {
+		
+	}
+
+}
